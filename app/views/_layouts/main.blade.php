cad3659a9971768b16e86372a5dfb5957088a613
GerasimenkoAlexander <gerasimenkoalexanderua@gmail.com>
2014-12-06 15:42:28 +0000

    Some styling and html

--- app/views/_layouts/main.blade.php
+++ app/views/_layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Programmaster</title>
     <link href="/components/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
     <link href="/css/main.css" rel="stylesheet">
     <script type="text/javascript" src="/components/jquery/dist/jquery.js"></script>
     <script type="text/javascript" src="/components/bootstrap/dist/js/bootstrap.js"></script>
@@ -18,6 +19,8 @@
     <script type="text/javascript" src="/components/snap.svg/dist/snap.svg-min.js"></script>
 </head>
 <body>
-    @yield('content')
+    <div class="container-fluid" id="main">
+        @yield('content')
+    </div>
 </body>
 </html>
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('_layouts.main')
 
 @section('content')
-    Programmaster
+    <div class="row-fluid">
+        <div class="col-md-4">
+            <input type="text" class="form-control" placeholder="I want to learn" id="search">
+        </div>
+    </div>
 @stop
