7178498e5faaf6f99ed68bb6bf3a00b4c1327c54
GerasimenkoAlexander <gerasimenkoalexanderua@gmail.com>
2014-12-06 15:17:19 +0000

    Add bower components and make start conf

---
+++ app/views/_layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Programmaster</title>
+    <link href="/components/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
+    <script type="text/javascript" src="/components/jquery/dist/jquery.js"></script>
+    <script type="text/javascript" src="/components/bootstrap/dist/js/bootstrap.js"></script>
+    <script type="text/javascript" src="/components/angular/angular.js"></script>
+    <script type="text/javascript" src="/components/angular-route/angular-route.js"></script>
+    <script type="text/javascript" src="/components/angular-resource/angular-resource.js"></script>
+    <script type="text/javascript" src="/components/angular-sortable-view/src/angular-sortable-view.js"></script>
+    <script type="text/javascript" src="/components/angular-contenteditable/angular-contenteditable.js"></script>
+    <script type="text/javascript" src="/components/ngUpload/ng-upload.js"></script>
+    <script type="text/javascript" src="/components/d3/d3.js"></script>
+    <script type="text/javascript" src="/components/snap.svg/dist/snap.svg-min.js"></script>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
